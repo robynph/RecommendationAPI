8bef53d8677162ffb41f40ad02ea39765cb702ef
MonikaAsawa <41234959+MonikaAsawa@users.noreply.github.com>
2018-09-12 17:44:50 -0400

    Add files via upload

---
+++ ProductRecommender.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 15:32:43 2018
+
+@author: Monika Asawa
+"""
+
+import pandas as pd
+import numpy as np
+
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+from sklearn.externals import joblib
+
+TOP_HOW_MANY = 3
+
+def encode_units(x):
+    if x <= 0:
+        return 0
+    if x >= 1:
+        return 1
+
+def generateRules():
+    
+    ordersDf = pd.read_csv('SuperStoreData.csv')
+    
+    'Focussing on Furniture Category only'
+    ordersDf = ordersDf.loc[ordersDf['Category'] == 'Furniture']
+    
+    orders =  pd.DataFrame(ordersDf, columns=['Order ID','Product Name'])
+    
+    print("Shape of orders data : ", np.shape(orders))
+    
+    'Removing the records which doesnt have OrderID'
+    orders.dropna(axis=0, subset=['Order ID'], inplace=True)
+    
+    basket = pd.crosstab(orders['Order ID'],orders['Product Name'])
+    
+    basket_sb = basket.applymap(encode_units)
+    
+    print("Shape of basket_sb data : ", np.shape(basket_sb))
+    
+    #----------------------------------------------------------------------
+    
+    'Generating frequent item sets that have a support of at least 0.1%'
+
+    frequent_itemsets = apriori(basket_sb, min_support=0.00001, use_colnames=True)
+
+    'Determining length of each itemset'
+    frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+    
+    print("Shape of frequent_itemsets data : ", np.shape(frequent_itemsets))
+    
+    #----------------------------------------------------------------------
+
+    'Generating the rules with their corresponding support, confidence and lift'
+    
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+    
+    print("Shape of rules data : ", np.shape(rules))
+    print("Printing top 5 rules generated", rules.head())
+    
+    'Creating new column to store value of consequents from frozen set format'
+    rules['conse'] = [list(x) for x in rules.consequents]
+    
+    
+    #----------------------------------------------------------------------
+    'Lift should be minimum 6 and confidence should be 80%'
+    rules = rules[ (rules['lift'] >= 6) & (rules['confidence'] >= 0.8)]
+    
+    'Converting lift(float) to lift(int)'
+    rules['lift'] = rules['lift'].apply(lambda x: int(x))
+    
+    return rules
+
+#----------------------------
+#rules = generateRules()
+#----------------------------
+
+
+
+#==============================================================================
+# Alternate Approach
+# import pickle
+# 
+# with open("apriori_product_recommender_pickle.pkl", "wb") as file_handler:
+#     pickle.dump(rules, file_handler)
+#     
+# with open("apriori_product_recommender_pickle.pkl", "rb") as file_handler:
+#     loaded_pickle = pickle.load(file_handler)
+#     
+# loaded_pickle
+#==============================================================================
+
+
+#from sklearn.externals import joblib
+#
+#joblib.dump(rules, "apriori_product_recommender.pkl")
+
+
+def recommend(selectedProducts):
+    
+    rules = joblib.load("./apriori_product_recommender.pkl")
+    suggestedProducts = recommendProducts(rules, selectedProducts)
+    
+    #Dummy value
+    discountPerc = 15
+    
+    return suggestedProducts,discountPerc
+
+def recommendProducts(rules, selectedProducts):
+    
+    orderedProducts = frozenset((selectedProducts))
+    
+    'extracting rules which has selected products as antecedents'
+    prodRules = rules[ rules['antecedents'] == orderedProducts ]
+    
+    print("Shape of prodRules data : ", np.shape(prodRules))
+    print("Printing top 5 prodRules generated", prodRules.head())
+    
+    'sorting out product rules by lift, confidence and support'
+    result = prodRules.sort_values(by=['lift', 'confidence','support'],axis=0, ascending=[0, 0, 0])
+    
+    print("Shape of result data : ", np.shape(result))
+    print("Printing top 5 result generated", result.head())
+    
+    'Determing best 3 rules for the selected products'
+    top_3_result = result[:TOP_HOW_MANY]
+    
+    conse = ""
+    
+    for i in top_3_result['conse']:
+    
+        print(i)
+        
+        for j in i:
+            print(j)
+            conse += j
+            
+            if(len(conse)>=3):
+                break
+        
+        if(len(conse)>=3):
+            break
+    
+    return conse
+
--- api_main.py
+++ api_main.py
@@ -4,13 +4,11 @@ Created on Mon Sep 10 18:24:13 2018
 
 @author: Monika Asawa
 """
-from flask import Flask, request
+from flask import Flask, request, jsonify
+
+from ProductRecommender import recommend
 
 app = Flask(__name__)
-from flask import jsonify
-from sklearn.externals import joblib
-import numpy as np
-
 
 @app.route("/")
 def hello():
@@ -24,70 +22,23 @@ TOP_HOW_MANY = 3
 
 
 @app.route("/recommend", methods=['POST'])
-def predict():
+def genRecommendation():
     if request.method == 'POST':
         try:
             data = request.get_json()
             print("Received value of data :: ",data)
             
-            selectedProducts = data["selectedProducts"]
+            selectedProducts = data["SelectedProducts"]
             if(len(selectedProducts)>0):
                 print("Received value of selectedProducts :: ",selectedProducts)
             else:
                 return jsonify({"Status" : "F", "Message" : "Please provide the selected product names."})
             
-            rules = joblib.load("./apriori_product_recommender.pkl")
-            
-            suggestedProducts = recommendProducts(rules, selectedProducts)
+            suggestedProducts,discountPerc = recommend(selectedProducts)
         
         except ValueError:
             return jsonify({"Status" : "F", "Message" : "Please provide the selected product names."})
 
-        return jsonify({"Status" : "S", "selectedProducts": suggestedProducts})
-
-#==============================================================================
-# def req_resp_int(selectedProducts):
-#     
-#     with open("apriori_product_recommender.pkl", "rb") as file_handler:
-#         rules = pickle.load(file_handler)
-#     
-#     return recommendProducts(rules, selectedProducts)
-#==============================================================================
+        return jsonify({"Status" : "S", "SuggestedProducts": suggestedProducts, "Discount" : discountPerc})
 
 
-def recommendProducts(rules, selectedProducts):
-    
-    orderedProducts = frozenset((selectedProducts))
-    
-    'extracting rules which has selected products as antecedents'
-    prodRules = rules[ rules['antecedents'] == orderedProducts ]
-    
-    print("Shape of prodRules data : ", np.shape(prodRules))
-    print("Printing top 5 prodRules generated", prodRules.head())
-    
-    'sorting out product rules by lift, confidence and support'
-    result = prodRules.sort_values(by=['lift', 'confidence','support'],axis=0, ascending=[0, 0, 0])
-    
-    print("Shape of result data : ", np.shape(result))
-    print("Printing top 5 result generated", result.head())
-    
-    'Determing best 3 rules for the selected products'
-    top_3_result = result[:TOP_HOW_MANY]
-    
-    conse = ""
-    
-    for i in top_3_result['conse']:
-    
-        print(i)
-        
-        for j in i:
-            print(j)
-            conse += j
-            
-            if(len(conse)>=3):
-                break
-        
-        if(len(conse)>=3):
-            break
-    
-    return conse
