043668d32897bbd997a4ba4668add761d8065b84
robynph <robyn@redh.io>
2018-09-17 15:47:48 -0400

    updated json

--- api_main.py
+++ api_main.py
@@ -20,42 +20,42 @@ if __name__ == '__main__':
 
 @app.route("/loadStoreData", methods=['POST','PUT'])
 def loadStoreData():
-    
+
     if request.method == 'POST':
         try:
-             
-            print("Received value of request :: ", request)  
-            
+
+            print("Received value of request :: ", request)
+
             req = request.get_json()
             print("Received value of req :: ", jsonify(req))
-            
+
             flag,value = validator(req, "app_id")
             if(flag):
                 app_id = value
                 print("Validated ",app_id)
             else:
                 return jsonify({"Status" : "F", "Message" : value})
-            
+
             flag,value = validator(req, "Orders")
             if(flag):
-                
+
                 orders = value
-                
+
                 for eachOrder in orders:
                         print(eachOrder["Ids"],eachOrder["line_items"])
-                        
+
                 print("Validated ",app_id)
             else:
                 return jsonify({"Status" : "F", "Message" : value})
 
-            
+
             message = ("Data for App Id {} uploaded successfully." .format(app_id))
-        
+
         except ValueError:
             return jsonify({"Status" : "F", "Message" : "Please provide the valid data for orders."})
 
         return jsonify({"Status" : "S","Message" : message})
-    
+
 
 @app.route("/recommend", methods=['POST'])
 def genRecommendation():
@@ -63,23 +63,23 @@ def genRecommendation():
         try:
             req = request.get_json()
             print("Received value of req :: ",req)
-            
-            
+
+
             flag,value = validator(req, "app_id")
             if(flag):
                 app_id = value
                 print("Validated ",app_id)
             else:
                 return jsonify({"Status" : "F", "Message" : value})
-            
+
             flag,value = validator(req, "Ids")
             if(flag):
                 orderId = value
                 print("Validated ",orderId)
             else:
                 return jsonify({"Status" : "F", "Message" : value})
-            
-            
+
+
             flag,value = validator(req, "SelectedProducts")
             if(flag):
                 selectedProducts = value
@@ -91,40 +91,40 @@ def genRecommendation():
             return jsonify({"Status" : "F", "Message" : "Please provide the selected product names."})
 
         return jsonify({"Status" : "S",  "Ids" : orderId ,"SuggestedProducts": suggestedProducts, "Discount" : discountPerc})
-       
-    
+
+
 @app.route("/feedback", methods=['POST'])
 def feedback():
-    
+
     try:
         req = request.get_json()
         print("Received value of req :: ",req)
-        
+
         flag,value = validator(req, "app_id")
         if(flag):
             app_id = value
             print("Validated ",app_id)
         else:
             return jsonify({"Status" : "F", "Message" : value})
-        
+
         flag,value = validator(req, "Ids")
         if(flag):
             orderId = value
             print("Validated ",orderId)
         else:
             return jsonify({"Status" : "F", "Message" : value})
-        
-        
+
+
         flag,value = validator(req, "SelectedProducts")
         if(flag):
             selectedProducts = value
-            
+
             'TO DO'
             'Write code here for reinforcement learning'
-            
+
         else:
             return jsonify({"Status" : "F", "Message" : value})
-    
+
     except ValueError:
         return jsonify({"Status" : "F", "Message" : "Please provide the valid data."})
 
@@ -134,65 +134,87 @@ def feedback():
 
 @app.route("/extra", methods=['POST','PUT'])
 def extra():
-    
+
     if request.method == 'POST':
         try:
-             
-            print("Received value of request :: ", request)  
-            
+
+            print("Received value of request :: ", request)
+
             req = request.get_json()
             print("Received value of req :: ", jsonify(req))
-            
-            
+
+
             f = request.files['files']
             if not f:
                 return "No file"
             #file_contents = StringIO(f.stream.read())
             print("Received file with file contents :: ",f)
-            
+
             reqJSON = request.files["json"]
             print("reqJSON :: " , reqJSON)
-            
+
             if(reqJSON["StoreID"]):
-                
+
                 storeID = req["StoreID"]
-                
+
                 if(len(storeID)>0):
                     print("Received value of StoreID :: ",storeID)
                 else:
                     return jsonify({"Status" : "F", "Message" : "StoreID parameter cannot be blank."})
-                
-            
+
+
             else:
                 return jsonify({"Status" : "F", "Message" : "StoreID parameter not passed"})
 
-        
+
         except ValueError:
             return jsonify({"Status" : "F", "Message" : "Please provide the selected product names."})
 
         return jsonify({"Status" : "S"})
-    
+
+
+@app.route("/qsetup", methods=['POST'])
+def qsetup():
+
+
+    if request.method == 'POST':
+        try:
+
+            print("Received request for questionnaire :: ", request)
+
+            req = request.get_json()
+            print("Received value of req :: ", jsonify(req))
+
+            flag,value = validator(req, "app_id")
+            if(flag):
+                app_id = value
+                print("Validated ",app_id)
+            else:
+                return jsonify({"Status" : "F", "Message" : value})
+
+        return jsonify({"app_id" : "App Id1" , "Questions" : [{"Q1" : "What is the maximum weight of each bundle?"},{"Q2" : "What is the maximum number of products to bundle?"},{"Q3" : "What is the flat discount to apply?"}]})
+        
 
 def validator(req, parameterName):
-    
+
     flag = False
     value = ""
-    
+
     print("Validating value received for ",parameterName)
-    
+
     if(req[parameterName]):
-        
+
         value = req[parameterName]
-                
+
         if(len(value)>0):
-            
+
             print("Received value of parameter :: ",value)
             flag = True
-        
+
         else:
             value = ('%s parameter cannot be blank.'.format(parameterName))
-    
+
     else:
         value = ('%s parameter not passed.'.format(parameterName))
-    
+
     return flag,value
