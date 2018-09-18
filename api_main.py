9316f60792ce7c91111b6a38b2336b3056f3401b
robynph <robyn@redh.io>
2018-09-18 02:53:55 -0400

    updated

--- api_main.py
+++ api_main.py
@@ -298,23 +298,20 @@ def confirmPurchase():
             req = request.get_json()
             print("Received value of req :: ", jsonify(req))
 
-            flag,value, confirm = validator(req, "app_id", "Purchased")
+            flag,value = validator(req, "app_id")
             if(flag):
                 app_id = value
-                Purchased = confirm
+
                 print("Validated ",app_id)
             else:
-                return jsonify({"Status" : "F", "Message" : value, "Purchased" : confirm})
+                return jsonify({"Status" : "F", "Message" : value})
 
             message = ("Data for App Id {} uploaded successfully." .format(app_id))
 
         except ValueError:
             return jsonify({"Status" : "F", "Message" : "Please provide the valid data for orders."})
 
-        return jsonify({"Status" : "S","Message" : message, "Purchased" : confirm})
-
-
-
+        return jsonify({"Status" : "S","Message" : message})
 
 
 
