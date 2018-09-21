9b36650af5ea430ff3349178325392a194791417
robynph <robyn@redh.io>
2018-09-21 09:22:27 -0400

    updated questions

--- api_main.py
+++ api_main.py
@@ -192,35 +192,7 @@ def qSetup():
                 return jsonify({"Status" : "F", "Message" : value})
 
             message = ("Data for App Id {} uploaded successfully." .format(app_id))
-            ques = ({"app_id" : app_id , "Questions" : [{
-    "Message": "Data for App Id test uploaded successfully.",
-    "Questions": {
-        "Questions": [
-            {
-                "Q1": "What will be the flat rate discount?",
-                "Type": "float",
-                "Range": "0-100",
-                "A1": "answer",
-            },
-            {
-                "Q2": "Which items (if any) will have modified discounts?",
-                "Type": "JSON (python dictionary)",
-                "Key": "string (ID) - product picker",
-                "Table Format": "polaris comp table to add new line"
-                "Value": "Type:float,Range:0-100",
-                "A2": "answer",
-            },
-            {
-                "Q3": "Which items will be exluded from any bundle?",
-                "Type": "stack, dynamic list"
-                "Key": "string (ID) - product picker"
-                "Table Format": "polaris comp table to add new line"
-                "A3": "answer",
-            }
-        ],
-        "app_id": "test"
-    },
-})
+            ques = ({"app_id" : app_id , "Questions":"will update"})
 
         except ValueError:
             return jsonify({"Status" : "F", "Message" : "Please provide the valid data for orders."})
