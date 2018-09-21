9d5789af633d631a54d1208e66454ede06403dd1
VirajM1290 <43383844+VirajM1290@users.noreply.github.com>
2018-09-21 12:29:49 -0400

    a

--- api_main.py
+++ api_main.py
@@ -94,11 +94,13 @@ def genRecommendation():
                 for eachProduct in selectedProducts:
                     variants = eachProduct['variants']
                     
+                    print("variants ::", variants)
+                    '''
                     for item in variants:
                         df.loc[count] = [item['sku']]
                         #tempList.append(df.loc[count])                              
                         count+=1
-                
+                    '''    
                 print(df)
                 print(tempList)
                 suggestedProducts,discountPerc = recommend(app_id,tempList)
